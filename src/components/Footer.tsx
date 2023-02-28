0a4824366a3ce1cb1a6aca0cf730af23a7e0791e
sou <ocelot2828@gmail.com>
2023-02-28 22:15:50 +0900

    Add: FooterStyles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,25 @@
+import Link from "next/link";
 import { Fragment } from "react";
+import styles from "styles/components/FooterStyles";
 
 const Footer = () => {
   return (
     <Fragment>
-      <div></div>
-      <style jsx></style>
+      <footer className="Fixed_bottom Font_size12px">
+        <div>
+          <Link href="/privacy">
+            <u>Privacy Policy</u>
+          </Link>{" "}
+          <a href="/rss.xml">
+            <u>RSS</u>
+          </a>
+          <br />
+          <Link href="/">© --- 2023</Link>
+        </div>
+      </footer>
+      <style jsx>{styles}</style>
     </Fragment>
   );
 };
+
 export default Footer;
---
+++ src/styles/components/FooterStyles.ts
@@ -0,0 +1,21 @@
+import css from "styled-jsx/css";
+
+const styles = css`
+  .Font_size12px {
+    font-size: 12px;
+  }
+  .Fixed_bottom {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    margin-bottom: 20px;
+    height: 30px;
+  }
+`;
+
+export default styles;
